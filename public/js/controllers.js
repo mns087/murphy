27751691ccada5ea80fc5146050c6bc76707db84
mns087 <mns087@gmail.com>
2015-05-05 22:15:55 +0100

    Add,Edit ROTA; Add,Edit Role; Add,Edit Location; Add,Edit Shift

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,6 @@
 var app = angular.module('murphy', []);
 
-app.controller('CreatePerson', function ($http) {
+app.controller('CreatePerson', function ($http, $rootScope) {
 
   var self = this;
   this.personDetail = {};
@@ -11,6 +11,7 @@ app.controller('CreatePerson', function ($http) {
     $http.get('api/person/all')
       .success(function (response) {
         self.persons = response.data;
+        $rootScope.rotaAutofill.persons = self.persons;
       });
   };
 
@@ -49,6 +50,266 @@ app.controller('CreatePerson', function ($http) {
   this.reset = function () {
     this.personDetail = {};
   };
+
+  this.get();
+});
+
+app.controller('RoleController', function ($http, $rootScope) {
+
+  var self = this;
+  this.roleDetail = {};
+  this.roles = [];
+
+  this.get = function () {
+    $http.get('api/role/all')
+      .success(function (response) {
+        self.roles = response.data;
+        $rootScope.rotaAutofill.roles = self.roles;
+      });
+  };
+
+  this.add = function () {
+
+    if (this.roleDetail.role_name === undefined) return;
+
+    $http.post('api/role/add', this.roleDetail)
+      .success(function (response) {
+        self.get();
+        self.reset();
+      })
+      .error(function (error) {});
+  };
+
+  this.update = function (role_index) {
+
+    $http.post('api/role/update', this.roles[role_index])
+      .success(function (response) {
+        self.roles[role_index].editmode = false;
+      })
+      .error(function (error) {});
+  };
+
+  this.delete = function (role_index) {
+
+    $http.post('api/role/delete', {
+        'role_id': self.roles[role_index].role_id
+      })
+      .success(function (response) {
+        self.roles.splice(role_index, 1);
+      })
+      .error(function (error) {});
+  };
+
+  this.reset = function () {
+    this.roleDetail = {};
+  };
+
+  this.get();
+
+});
+
+app.controller('ShiftController', function ($http, $rootScope) {
+
+  var self = this;
+  this.shiftDetail = {};
+  this.shifts = [];
+
+  this.get = function () {
+    $http.get('api/shift/all')
+      .success(function (response) {
+        self.shifts = response.data;
+        $rootScope.rotaAutofill.shifts = self.shifts;
+      });
+  };
+
+  this.add = function () {
+
+    if (this.shiftDetail.shift_name === undefined) return;
+
+    $http.post('api/shift/add', this.shiftDetail)
+      .success(function (response) {
+        self.get();
+        self.reset();
+      })
+      .error(function (error) {});
+  };
+
+  this.update = function (shift_index) {
+
+    $http.post('api/shift/update', this.shifts[shift_index])
+      .success(function (response) {
+        self.shifts[shift_index].editmode = false;
+      })
+      .error(function (error) {});
+  };
+
+  this.delete = function (shift_index) {
+
+    $http.post('api/shift/delete', {
+        'shift_id': self.shifts[shift_index].shift_id
+      })
+      .success(function (response) {
+        self.shifts.splice(shift_index, 1);
+      })
+      .error(function (error) {});
+  };
+
+  this.reset = function () {
+    this.shiftDetail = {};
+  };
+
+  this.get();
+
+});
+
+app.controller('LocationController', function ($http, $rootScope) {
+
+  var self = this;
+  this.locationDetail = {};
+  this.locations = [];
+
+  this.get = function () {
+    $http.get('api/location/all')
+      .success(function (response) {
+        self.locations = response.data;
+        $rootScope.rotaAutofill.locations = self.locations;
+      });
+  };
+
+  this.add = function () {
+
+    if (this.locationDetail.location_name === undefined) return;
+
+    $http.post('api/location/add', this.locationDetail)
+      .success(function (response) {
+        self.get();
+        self.reset();
+      })
+      .error(function (error) {});
+  };
+
+  this.update = function (location_index) {
+
+    $http.post('api/location/update', this.locations[location_index])
+      .success(function (response) {
+        self.locations[location_index].editmode = false;
+      })
+      .error(function (error) {});
+  };
+
+  this.delete = function (location_index) {
+
+    $http.post('api/location/delete', {
+        'location_id': self.locations[location_index].location_id
+      })
+      .success(function (response) {
+        self.locations.splice(location_index, 1);
+      })
+      .error(function (error) {});
+  };
+
+  this.reset = function () {
+    this.locationDetail = {};
+  };
+
+  this.get();
+
+});
+
+app.controller('RotaController', function ($http, $rootScope) {
+
+  $rootScope.rotaAutofill = {};
+  var self = this;
+  this.rotaDetail = {
+    "rows" : [{
+      "person_id" : '',
+      "role_id" : '',
+      "location_id" : '',
+      "shift_id" : ''
+    }]
+  };
+  this.rows = [1];
+  this.rotas = [];
+
+  $rootScope.$watchCollection('rotaDetail', function () {
+    self.rotaAutofill = $rootScope.rotaAutofill;
+  });
+
+  this.get = function () {
+    $http.get('api/rota/all')
+      .success(function (response) {
+        self.rotas = response.data;
+      });
+  };
+
+  this.add = function () {
+
+    if (this.rotaDetail.rota_name === undefined) return;
+
+    $http.post('api/rota/add', this.rotaDetail)
+      .success(function (response) {
+        self.get();
+        self.reset();
+      })
+      .error(function (error) {});
+  };
+
+  this.edit = function (rota_index) {
+    this.editmode = true;
+    this.rotaDetail.rota_id = this.rotas[rota_index].rota_id;
+    this.rotaDetail.rota_name = this.rotas[rota_index].rota_name;
+    this.rotaDetail.rota_desc = this.rotas[rota_index].rota_desc;
+    
+    $http.get('api/rota/all/' + this.rotas[rota_index].rota_id)
+      .success(function (response) {
+        self.rotaDetail.rows = response.data;
+      })
+      .error(function(error){});
+      
+  };
+
+  this.update = function (rota_id) {
+
+    $http.post('api/rota/update', self.rotaDetail)
+      .success(function (response) {
+        self.reset();
+        self.get();
+      })
+      .error(function (error) {});
+
+  };
+
+  this.delete = function (rota_index) {
+
+    $http.post('api/rota/delete', {
+        'rota_id': self.rotas[rota_index].rota_id
+      })
+      .success(function (response) {
+        self.rotas.splice(rota_index, 1);
+      })
+      .error(function (error) {});
+  };
+
+  this.reset = function () {
+    this.editmode = false;
+    this.rotaDetail = {
+      "rows" : [{
+        "person_id" : "",
+        "role_id" : "",
+        "location_id" : "",
+        "shift_id" : ""
+      }]
+    };
+  };
+
+  this.addRow = function () {
+    this.rotaDetail.rows.push({
+      "person_id" : "",
+      "role_id" : "",
+      "location_id" : "",
+      "shift_id" : ""
+    });
+  }
 
   this.get();
 });
--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,9 @@ function connectDB() {
     database: 'murphy'
   });
   //mysql -u admineajrzyG -pduXjWWy5GKh7 -h 127.5.120.130 murphy 
+  //For local mysql, Start mysql from system preferenece on mac.
+  //export PATH=${PATH}:/usr/local/mysql/bin
+
   db.connect(function (err) {
     if (err) {
       throw err;
@@ -72,5 +75,253 @@ router.post('/person/update', function (req, res, next) {
     }
   });
 });
+
+
+/* ADD role listing. */
+router.post('/role/add', function (req, res, next) {
+
+  db.query("INSERT INTO role (role_name, role_desc) VALUES ('" + req.body.role_name + "', '" + (req.body.role_desc || "") + "')", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success"}');
+    }
+  });
+});
+
+router.get('/role/all', function (req, res, next) {
+
+  db.query("SELECT * FROM role WHERE 1 ORDER BY role_id DESC", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success", "data" : ' + JSON.stringify(rows) + '}');
+    }
+  });
+});
+
+router.post('/role/delete', function (req, res, next) {
+
+  db.query("DELETE FROM role WHERE role_id = '" + req.body.role_id + "'", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success"}');
+    }
+  });
+});
+
+router.post('/role/update', function (req, res, next) {
+
+  db.query("UPDATE role SET role_name='" + req.body.role_name + "', role_desc='" + req.body.role_desc + "' WHERE role_id = '" + req.body.role_id + "'", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success"}');
+    }
+  });
+});
+
+
+/* ADD shift listing. */
+router.post('/shift/add', function (req, res, next) {
+
+  db.query("INSERT INTO shift (shift_name, shift_desc) VALUES ('" + req.body.shift_name + "', '" + (req.body.shift_desc || "") + "')", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success"}');
+    }
+  });
+});
+
+router.get('/shift/all', function (req, res, next) {
+
+  db.query("SELECT * FROM shift WHERE 1 ORDER BY shift_id DESC", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success", "data" : ' + JSON.stringify(rows) + '}');
+    }
+  });
+});
+
+router.post('/shift/delete', function (req, res, next) {
+
+  db.query("DELETE FROM shift WHERE shift_id = '" + req.body.shift_id + "'", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success"}');
+    }
+  });
+});
+
+router.post('/shift/update', function (req, res, next) {
+
+  db.query("UPDATE shift SET shift_name='" + req.body.shift_name + "', shift_desc='" + req.body.shift_desc + "' WHERE shift_id = '" + req.body.shift_id + "'", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success"}');
+    }
+  });
+});
+
+
+/* ADD location listing. */
+router.post('/location/add', function (req, res, next) {
+
+  db.query("INSERT INTO location (location_name, location_desc) VALUES ('" + req.body.location_name + "', '" + (req.body.location_desc || "") + "')", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success"}');
+    }
+  });
+});
+
+router.get('/location/all', function (req, res, next) {
+
+  db.query("SELECT * FROM location WHERE 1 ORDER BY location_id DESC", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success", "data" : ' + JSON.stringify(rows) + '}');
+    }
+  });
+});
+
+router.post('/location/delete', function (req, res, next) {
+
+  db.query("DELETE FROM location WHERE location_id = '" + req.body.location_id + "'", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success"}');
+    }
+  });
+});
+
+router.post('/location/update', function (req, res, next) {
+
+  db.query("UPDATE location SET location_name='" + req.body.location_name + "', location_desc='" + req.body.location_desc + "' WHERE location_id = '" + req.body.location_id + "'", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success"}');
+    }
+  });
+});
+
+
+/* ADD rota listing. */
+router.post('/rota/add', function (req, res, next) {
+
+  db.query("INSERT INTO rota (rota_name, rota_desc) VALUES ('" + req.body.rota_name + "', '" + (req.body.rota_desc || "") + "')", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      var values = [];
+      for (var i=0; i<req.body.rows.length; i++) {
+        values[i] = [rows.insertId, req.body.rows[i].person_id, req.body.rows[i].role_id, req.body.rows[i].location_id, req.body.rows[i].shift_id];
+      }
+      
+      db.query("INSERT INTO rota_mapper (rota_id, person_id, role_id, location_id, shift_id) VALUES ?", [values], function(err) {
+        if (err) {
+          res.send('{"status": "error"}');
+        } else {
+          res.send('{"status": "success"}');
+        }
+      });
+    }
+  });
+});
+
+router.get('/rota/all', function (req, res, next) {
+
+  db.query("SELECT * FROM rota WHERE 1 ORDER BY rota_id DESC", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success", "data" : ' + JSON.stringify(rows) + '}');
+    }
+  });
+});
+
+router.get('/rota/all/:id', function (req, res, next) {
+
+  db.query("SELECT * FROM rota_mapper WHERE rota_id = '"+ req.params.id +"'", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      res.send('{"status": "success", "data" : ' + JSON.stringify(rows) + '}');
+    }
+  });
+});
+
+
+router.post('/rota/delete', function (req, res, next) {
+
+  db.query("DELETE FROM rota WHERE rota_id = '" + req.body.rota_id + "'", function (err, rows, fields) {
+    if (err) {
+      throw err;
+      res.send('{"status": "error"}');
+    } else {
+      db.query("DELETE FROM rota_mapper WHERE rota_id = '" + req.body.rota_id + "'", function (err, rows, fields) {
+        if (err) {
+          throw err;
+          res.send('{"status": "error"}');
+        } else {
+          res.send('{"status": "success"}');
+        }
+      });
+    }
+  });
+});
+
+router.post('/rota/update', function (req, res, next) {
+
+  /*
+  var sql = "UPDATE rota SET rota_name='" + req.body.rota_name + "', rota_desc='" + req.body.rota_desc + "' WHERE rota_id = '" + req.body.rota_id + "';";
+  for (var i=0; i<req.body.rows.length; i++) {
+    sql = sql + db.format("UPDATE rota_mapper SET ? WHERE rota_mapper_id = ?;", [req.body.rows[i], req.body.rows[i].rota_mapper_id]);
+      //sql = sql + "UPDATE rota_mapper SET rota_id = "+req.body.rows[i].rota_id+", person_id = "+req.body.rows[i].person_id+", shift_id = "+req.body.rows[i].shift_id+", location_id = "+req.body.rows[i].location_id+",  WHERE rota_mapper_id = "+req.body.rows[i].shift_id+";"
+  }
+  */
+  db.query("UPDATE rota SET rota_name='" + req.body.rota_name + "', rota_desc='" + req.body.rota_desc + "' WHERE rota_id = '" + req.body.rota_id + "'", function () {
+    db.query("DELETE FROM rota_mapper WHERE rota_id = '" + req.body.rows[0].rota_id + "'", function (err, rows, fields) {
+      var values = [];
+      for (var i=0; i<req.body.rows.length; i++) {
+        values[i] = [req.body.rota_id, req.body.rows[i].person_id, req.body.rows[i].role_id, req.body.rows[i].location_id, req.body.rows[i].shift_id];
+      }
+      
+      db.query("INSERT INTO rota_mapper (rota_id, person_id, role_id, location_id, shift_id) VALUES ?", [values], function(err) {
+        if (err) {
+          res.send('{"status": "error"}');
+        } else {
+          res.send('{"status": "success"}');
+        }
+      });
+    });
+  });
+});
+
+
 
 module.exports = router;
